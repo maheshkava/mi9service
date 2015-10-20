ac0d8332761d0009637a4dc00d74b0e810482b3e
maheshkava <maheshkava@gmail.com>
2015-10-21 09:44:14 +1100

    routing fix

--- mi9service/App_Start/WebApiConfig.cs
+++ mi9service/App_Start/WebApiConfig.cs
@@ -5,24 +5,29 @@ using System.Web.Http;
 
 namespace mi9service
 {
+    /// <summary>
+    /// Web Api Configuration class
+    /// </summary>
     public static class WebApiConfig
     {
+        /// <summary>
+        /// Registers the configuration
+        /// Here Cross-origin resource sharing needs to be enabled.
+        /// Default routing to the API is also configured here.
+        /// </summary>
+        /// <param name="config">HttpConfiguration</param>
         public static void Register(HttpConfiguration config)
         {
+            //Cross-origin resource sharing 
+            config.EnableCors();
+
+            //Default Routing
+            config.MapHttpAttributeRoutes();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "{controller}/{id}",
+                defaults: new { controller = "service", id = RouteParameter.Optional }
             );
-
-            // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
-            // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
-            // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
-            //config.EnableQuerySupport();
-
-            // To disable tracing in your application, please comment out or remove the following line of code
-            // For more information, refer to: http://www.asp.net/web-api
-            config.EnableSystemDiagnosticsTracing();
         }
     }
 }
