844ee3270eadc3e5403e0e6ab0970aecffd2cb69
maheshkava <maheshkava@gmail.com>
2015-10-20 14:33:35 +1100

    build fix

--- mi9service/Global.asax.cs
+++ mi9service/Global.asax.cs
@@ -16,12 +16,7 @@ namespace mi9service
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
